a4f9dc1dd0d20de484f0db28312787ac6b3a4aea
Petar Bozinoski <petar.bozinoski@hotmail.com>
2023-03-06 19:32:42 +0100

    Update Program.cs

--- Homeworks/Homework2/Task1/Task1/Program.cs
+++ Homeworks/Homework2/Task1/Task1/Program.cs
@@ -13,6 +13,9 @@ foreach(int num in array)
     if(num % 2 == 0)
     {
         sum += num;
+    }else
+    {
+        Console.WriteLine("Invalid input!");
     }
 }
 Console.WriteLine($"The result is: {sum}");

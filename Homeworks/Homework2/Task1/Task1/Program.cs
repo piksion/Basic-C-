1e15968383d2b2f050da93aea6b4ac6b75101693
Petar Bozinoski <petar.bozinoski@hotmail.com>
2023-03-06 18:08:04 +0100

    Add files via upload

---
+++ Homeworks/Homework2/Task1/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int[6];
+int sum = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Enter integer no.{i + 1}: ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+foreach(int num in array)
+{
+    if(num % 2 == 0)
+    {
+        sum += num;
+    }
+}
+Console.WriteLine($"The result is: {sum}");
+
---
+++ Homeworks/Homework2/Task2/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] G1 = new string[] { "Petar", "Jovana", "Andrijana", "Ivan", "Sergej" };
+string[] G2 = new string[] { "Rakida", "Razmena", "Borka", "Zorka", "Menka" };
+
+Console.Write("Enter student group(there are 1 and 2): ");
+int input = int.Parse(Console.ReadLine());
+
+if (input == 1)
+{
+    Console.WriteLine("The students in G1 are: ");
+    foreach (string student in G1)
+    {
+        Console.WriteLine(student);
+    }
+}
+else if (input == 2)
+{
+    Console.WriteLine("The students in G2 are: ");
+    foreach (string student in G2)
+    {
+        Console.WriteLine(student);
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input!");
+}

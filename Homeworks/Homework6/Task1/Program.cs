4af771a3ed2ede330766c956286fae7ca34aa23e
Petar Bozinoski <petar.bozinoski@hotmail.com>
2023-03-21 14:56:05 +0100

    Add files via upload

---
+++ Homeworks/Homework6/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿Queue<int> numbersQueue = new Queue<int>();
+bool continute = true;
+while (continute)
+{
+    Console.WriteLine("Enter a number: ");
+    int numInput = int.Parse(Console.ReadLine());
+    Console.WriteLine("Do you want to continue? Press 'Y' to continue or 'N' to cancel!");
+    string userInput = Console.ReadLine().ToUpper();
+    numbersQueue.Enqueue(numInput);
+    if (userInput == "N")
+    {
+        Console.Write("Showing numbers in the Queue: \n");
+        foreach(int num in  numbersQueue) 
+        {
+            Console.WriteLine(num + " ");
+        }
+        
+        break;
+    }
+}
